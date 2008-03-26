ab00a0a0da21034defb06a2ba4b1529520d7868c
Martin 'Kingpin' Thurn <kingpin@dcswcc.org>
2008-03-26 04:01:21 +0000

    Import of MTHURN/WWW-Search-Yahoo-China-1.001 from CPAN.
    gitpan-cpan-distribution: WWW-Search-Yahoo-China
    gitpan-cpan-version:      1.001
    gitpan-cpan-path:         MTHURN/WWW-Search-Yahoo-China-1.001.tar.gz
    gitpan-cpan-author:       MTHURN
    gitpan-cpan-maturity:     released

---
+++ lib/WWW/Search/Yahoo/China.pm
@@ -0,0 +1,169 @@
+
+# $Id: China.pm,v 2.11 2008/03/26 03:22:55 Martin Exp $
+
+=head1 NAME
+
+WWW::Search::Yahoo::China - class for searching Yahoo! China
+
+=head1 SYNOPSIS
+
+  use WWW::Search;
+  my $oSearch = new WWW::Search('Yahoo::China');
+  my $sQuery = WWW::Search::escape_query("暴囂勧秤");
+  $oSearch->native_query($sQuery);
+  while (my $oResult = $oSearch->next_result())
+    print $oResult->url, "\n";
+
+=head1 DESCRIPTION
+
+This class is a Yahoo! China specialization of L<WWW::Search>.  It
+handles making and interpreting searches on Yahoo! China
+F<http://cn.yahoo.com>.
+
+This class exports no public interface; all interaction should
+be done through L<WWW::Search> objects.
+
+=head1 NOTES
+
+I have no idea what character encoding(s) are accepted/expected by
+Yahoo China's website.  To create/test this backend I just
+cut-and-pasted Chinese characters from cn.yahoo.com.
+
+It seems that if your query is in UTF-8, sending option {ei =>
+'UTF-8'} makes it do the right thing.
+
+=head1 SEE ALSO
+
+To make new back-ends, see L<WWW::Search>.
+
+=head1 BUGS
+
+Please tell the maintainer if you find any!
+
+=head1 AUTHOR
+
+C<WWW::Search::Yahoo> is maintained by Martin Thurn
+(mthurn@cpan.org).
+
+=head1 LEGALESE
+
+THIS SOFTWARE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
+=cut
+
+package WWW::Search::Yahoo::China;
+
+use strict;
+use warnings;
+
+use Data::Dumper;  # for debugging only
+use WWW::Search::Yahoo 2.372;
+use base 'WWW::Search::Yahoo';
+
+our
+$VERSION = do { my @r = (q$Revision: 2.11 $ =~ /\d+/g); sprintf "%d."."%03d" x $#r, @r };
+our $MAINTAINER = 'Martin Thurn <mthurn@cpan.org>';
+
+sub native_setup_search
+  {
+  my ($self, $sQuery, $rh) = @_;
+  $self->{'_options'} = {
+                         'p' => $sQuery,
+                         'scch' => 'on',
+                         'stype' => '',
+                        };
+  $rh->{'search_base_url'} = 'http://cn.search.yahoo.com';
+  $rh->{'search_base_path'} = '/search/cn';
+  return $self->SUPER::native_setup_search($sQuery, $rh);
+  } # native_setup_search
+
+sub _where_to_find_count
+  {
+  my %hash = (
+              _tag => 'span',
+              class => 'num',
+             );
+  return \%hash;
+  } # _where_to_find_count
+
+sub _string_has_count
+  {
+  my $self = shift;
+  my $s = shift;
+  # I NEED A CHINESE READER TO SEND ME THE CORRECT PATTERN.
+  return $1 if ($s =~ m!([,0-9]+)[^0-9]*\z!i);
+  return -1;
+  } # _string_has_count
+
+sub _result_list_tags
+  {
+  return (
+          _tag => 'div',
+          class => 'i',
+         );
+  } # _result_list_tags
+
+sub _result_list_items
+  {
+  my $self = shift;
+  my $oTree = shift || die;
+  my $oDIV = $oTree->look_down(
+                               _tag => 'div',
+                               class => 'rst'
+                              );
+  return () if ! ref $oDIV;
+  my @ao = $oDIV->look_down(_tag => 'li');
+  return @ao;
+  } # _result_list_items
+
+sub _a_is_next_link
+  {
+  my $self = shift;
+  my $oA = shift;
+  return 0 unless (ref $oA);
+  # I can not type Chinese, nor even cut-and-paste into Emacs with
+  # confidence that the encoding will not get screwed up, so I resort
+  # to this ugliness:
+  return ($oA->as_HTML =~ m!&Iuml;&Acirc;&Ograve;&raquo;&Ograve;&sup3;!i);
+  } # _a_is_next_link
+
+sub parse_details
+  {
+  my $self = shift;
+  # Required arg1 = (part of) an HTML parse tree:
+  my $oLI = shift;
+  # Required arg2 = a WWW::SearchResult object to fill in:
+  my $hit = shift;
+  # Grab the size & date:
+  my $oDIV = $oLI->look_down(_tag => 'span',
+                            );
+  if (ref $oDIV)
+    {
+    my $sDIV = $oDIV->as_text;
+    $sDIV =~ s!\240! !g;
+    print STDERR " DDD Y::C found rel div =$sDIV=\n" if (2 <= $self->{_debug});
+    if ($sDIV =~ m! - ([0-9KM]+) - !)
+      {
+      $hit->size($1);
+      } # if
+    if ($sDIV =~ m!/ / / ([-0-9/]+)/ / / !)
+      {
+      $hit->change_date($1);
+      } # if
+    $oDIV->detach;
+    $oDIV->delete;
+    } # if
+  my $oTR = $oLI->look_down(_tag => 'p',
+                           );
+  if (ref $oTR)
+    {
+    $hit->description($oTR->as_text);
+    } # if
+  } # parse_details
+
+1;
+
+__END__
+
---
+++ t/china.t
@@ -0,0 +1,64 @@
+use ExtUtils::testlib;
+use Test::More no_plan;
+
+BEGIN { use_ok('WWW::Search') };
+BEGIN { use_ok('WWW::Search::Test') };
+BEGIN { use_ok('WWW::Search::Yahoo') };
+BEGIN { use_ok('WWW::Search::Yahoo::China') };
+
+&tm_new_engine('Yahoo::China');
+my $iDebug = 0;
+my $iDump = 0;
+
+# goto TEST_NOW;
+# goto MULTI_TEST;
+
+# This test returns no results (but we should not get an HTTP error):
+diag("Sending 0-page query to cn.yahoo.com...");
+$iDebug = 0;
+$iDump = 0;
+&tm_run_test('normal', $WWW::Search::Test::bogus_query, 0, 0, $iDebug, $iDump);
+# goto ALL_DONE; # for testing
+TEST_NOW:
+$iDebug = 0;
+$iDump = 0;
+# This query returns 1 page of results:
+diag("Sending 1-page query to cn.yahoo.com...");
+TODO:
+  {
+  $TODO = q{I need a Chinese reader to implement the result-count regex};
+  &tm_run_test('normal',
+               # "\xE8\xAF\xB7\xE4\xB9\xA6\xE7\x9A\x84\xE5\x86\x99\xE6\xB3\x95",
+               'wiz'.'erdry',
+               1, 99, $iDebug, $iDump, {ei => 'UTF-8'});
+  $TODO = '';
+  } # end of TODO block
+my @ao = $WWW::Search::Test::oSearch->results();
+cmp_ok(0, '<', scalar(@ao), 'got any results');
+foreach my $oResult (@ao)
+  {
+  like($oResult->url, qr{\Ahttp://},
+       'result URL is http');
+  cmp_ok($oResult->title, 'ne', '',
+         'result Title is not empty');
+  cmp_ok($oResult->description, 'ne', '',
+         'result description is not empty');
+  } # foreach
+# goto ALL_DONE;
+MULTI_TEST:
+diag("Sending multi-page query to cn.yahoo.com...");
+$iDebug = 0;
+$iDump = 0;
+# This query returns MANY pages of results:
+TODO:
+  {
+  $TODO = q{I need a Chinese reader to implement the result-count regex};
+  &tm_run_test('normal', "\xCB\xBD", 21, undef, $iDebug, $iDump);
+  $TODO = '';
+  } # end of TODO block
+
+ALL_DONE:
+exit 0;
+
+__END__
+
